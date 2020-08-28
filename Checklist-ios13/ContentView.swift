eb7415ec1776bafe2a1bf976b8b17490e429a850
safarsafarov <safarov.safar@hotmail.com>
2020-08-29 00:07:08 +0800

    Checklist-ios13

--- Checklist-ios13/ContentView.swift
+++ Checklist-ios13/ContentView.swift
@@ -1,16 +1,8 @@
-//
-//  ContentView.swift
-//  Checklist-ios13
-//
-//  Created by Safar Safarov on 2020/8/29.
-//  Copyright © 2020 safarslife.dev. All rights reserved.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Good one!")
+        Text("Good Another one!")
     }
 }
 
