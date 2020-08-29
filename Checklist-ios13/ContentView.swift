5ca3683a3ddb0f0a10e49e143a8267a377e5b5de
safarsafarov <safarov.safar@hotmail.com>
2020-08-29 22:22:02 +0800

    Checklist-ios13

--- Checklist-ios13/ContentView.swift
+++ Checklist-ios13/ContentView.swift
@@ -2,20 +2,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        List {
-            Section(header: Text("High priority")) {
-                Text("Walk the dog")
-                Text("Brush my teeth")
-                Text("Learn iOS development")
-                Text("Soccer practice")
-                Text("Eat ice cream")
-            }
-            Section(header: Text("Low priority")) {
-                Text("Soccer practice")
-                Text("Eat ice cream")
-            }
-        }
-        .navigationBarTitle("CheckList")
+       NavigationView {
+           List {
+               Text("Walk thee dog")
+               Text("Brush my teeth")
+               Text("Learn iOS development")
+               Text("Soccer practice")
+               Text("Eat ice cream")
+           }
+           .navigationBarTitle("Checklist")
+       }
     }
 
 }
